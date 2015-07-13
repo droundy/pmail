dfb4c610a51978a0dd38dfa945d986bc78c98c6c
David Roundy <roundyd@physics.oregonstate.edu>
2015-07-13 08:15:41 -0400

    add button and switch to material design

--- src/org/abridgegame/messy/Messy.java
+++ src/org/abridgegame/messy/Messy.java
@@ -2,14 +2,26 @@ package org.abridgegame.messy;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
 
 public class Messy extends Activity
 {
+    public final static String EXTRA_MESSAGE = "org.abridgegame.messy.MESSAGE";
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        //Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        //intent.putExtra(EXTRA_MESSAGE, message);
     }
 }
