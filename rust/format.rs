6fd8604b792254400a3d649e7055d74d606b90fc
Steven Allen <steven@stebalien.com>
2016-06-14 09:44:07 -0400

    update to tempfile 2

--- rust/format.rs
+++ rust/format.rs
@@ -79,7 +79,7 @@ mod test {
             time: super::epoch_to_rfc3339(137),
         };
 
-        let mut f = tempfile::TempFile::new().unwrap();
+        let mut f = tempfile::tempfile().unwrap();
         serde_json::to_writer(&mut f, &x).unwrap();
 
         f.seek(std::io::SeekFrom::Start(0)).unwrap();
